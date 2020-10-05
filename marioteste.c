c081203f260bf56128e9d4d9e892221537de5c11
GiBastos <gimeiodigital@gmail.com>
2020-10-05 03:59:45 +0000

    new code

---
+++ marioteste.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int n;
+
+  printf("Qual a altura da pirâmide? ");
+  scanf("%i",&n);
+
+  printf("Sua pirâmide tem %i metros de altura", n);
+
+  return 0;
+}
